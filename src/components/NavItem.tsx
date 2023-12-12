e1669b49ecac3331a1eda55831dff0d448a5bcb6
PrAndrian <princydruh@gmail.com>
2023-12-12 16:01:27 +0100

    link marketplace navbar items

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -55,25 +55,28 @@ const NavItem = ({
                       key={item.name}
                       className='group relative text-base sm:text-sm'
                     >
-                      <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
-                        <Image
-                          src={item.imageSrc}
-                          alt='product category image'
-                          fill
-                          className='object-cover object-center'
-                        />
-                      </div>
+                      <Link href={item.href}>
+                        <div className='relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
+                          <Image
+                            src={item.imageSrc}
+                            alt='product category image'
+                            fill
+                            className='object-cover object-center'
+                          />
+                        </div>
 
-                      <Link
-                        href={item.href}
-                        className='mt-6 block font-medium text-gray-900'>
-                        {item.name}
+                        <span
+                          className='mt-6 block font-medium text-gray-900'
+                        >
+                          {item.name}
+                        </span>
+                        <p
+                          className='mt-1'
+                          aria-hidden='true'
+                        >
+                          Shop now
+                        </p>
                       </Link>
-                      <p
-                        className='mt-1'
-                        aria-hidden='true'>
-                        Shop now
-                      </p>
                     </div>
                   ))}
                 </div>
