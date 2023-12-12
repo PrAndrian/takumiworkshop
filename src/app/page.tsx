9b26ad50f1769d8b7827e86530606ab4fec23aaa
PrAndrian <princydruh@gmail.com>
2023-12-12 12:18:45 +0100

    landing page hero ongoing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1></h1>
+        <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
+          Your marketplace for forged {' '}
+          <span className="text-pink-600">
+            anime items
+          </span>
+          .
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to Takumi workshop. Every items are 
+          verified by our team to ensure our highest 
+          quality standards.  
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>
+            Browse Trending
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
